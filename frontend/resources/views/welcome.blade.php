9be6b2e37133124ba9726a6ffff902c69f9a07a3
Chris <cb20032014@gmail.com>
2024-12-05 16:03:56 +0000

    config tailwind css

--- frontend/resources/views/welcome.blade.php
+++ frontend/resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <title>Laravel</title>
 
@@ -16,6 +17,7 @@
         </style>
     </head>
     <body class="antialiased">
+
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
